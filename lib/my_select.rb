8e645ed3dba0f7fccd8fd77ed9c35b32f9f9888b
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-23 11:17:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ new_collection = []
  i = 0 
 
  while i < collection.length 
- new_collection << collection.select { |i| yield[i] }  
+ new_collection << collection.select { |i| yield(i) }  
   i += 1 
  end 
  
